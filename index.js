5a5685030a95065f27b44bc62ccfb9354c7ba27a
Lukman Hossain <lukman.webdev@gmail.com>
2021-11-11 18:10:38 +0600

    server side connect commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const { MongoClient } = require("mongodb");
+// const ObjectId = require("mongodb").ObjectId;
+
+const cors = require('cors');
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iuax9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('database connect');
+    }
+    finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`listening at ${port}`)
+})
